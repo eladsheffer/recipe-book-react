d1924ed4ff0dfc08e45bebd6713344891f395ed6
mastik8 <mastik8@gmail.com>
2021-10-17 20:41:17 +0300

    Updates

---
+++ precache-manifest.02df9c2e1b0e20db2654d65938d1c78d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc75f9c394946900f725767a5b48223",
+    "url": "/recipe-book-react/index.html"
+  },
+  {
+    "revision": "519dff7a9e4234e4ed5f",
+    "url": "/recipe-book-react/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "6923ea98a8da43797dea",
+    "url": "/recipe-book-react/static/css/main.80654033.chunk.css"
+  },
+  {
+    "revision": "519dff7a9e4234e4ed5f",
+    "url": "/recipe-book-react/static/js/2.a17a8fd2.chunk.js"
+  },
+  {
+    "revision": "6923ea98a8da43797dea",
+    "url": "/recipe-book-react/static/js/main.1b7bd5c6.chunk.js"
+  },
+  {
+    "revision": "a2c3b617f57ca5ade380",
+    "url": "/recipe-book-react/static/js/runtime-main.0fb45ccf.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/recipe-book-react/static/media/logo.25bf045c.svg"
+  }
+]);
